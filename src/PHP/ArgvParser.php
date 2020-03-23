f849f9117a8d07658a6ac39a1894bd92205c28aa
Roy Arisse <mail@royarisse.com>
2020-03-23 17:31:45 +0100

    Allow single character argument list
    A single character argument list like "-ok" is properly parsed as `[o => true, k => true]` instead of `[ok => true]`.
    Off course behaviour of "--ok" remains unchanged.

--- src/PHP/ArgvParser.php
+++ src/PHP/ArgvParser.php
@@ -39,6 +39,10 @@ class PHP_ArgvParser
                     // have sub parameter
                     $configs[$matches[1]] = $argv[$index + 1];
                     $index++;
+                } elseif (strpos($matches[0], '--') === false) {
+                    for ($j = 0; $j < strlen($matches[1]); $j += 1) {
+                        $configs[$matches[1][$j]] = true;
+                    }
                 } else {
                     $configs[$matches[1]] = true;
                 }
