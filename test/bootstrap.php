5648c84997bfaa9c3c04ef48d9633ce9af2aba35
SJ Chou <public.sj@gmail.com>
2020-03-26 09:00:06 +0800

    try to fix travis-ci test

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 include_once __DIR__ . '/../src/PHP/ArgvParser.php';
+include_once __DIR__ . '/../vendor/autoload.php';
 
+// backward compatibility
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase'))
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
